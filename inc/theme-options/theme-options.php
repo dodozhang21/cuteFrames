24faa478e6df204c089bf39333cfb2fefa5f94d4
Ying Zhang <dodozhang21@gmail.com>
2013-05-20 19:05:32 -0500

    latest changes

--- inc/theme-options/theme-options.php
+++ inc/theme-options/theme-options.php
@@ -7,29 +7,29 @@
  */
 
 /**
- * Register the form setting for our oriental_options array.
+ * Register the form setting for our cuteFrames_options array.
  *
  * This function is attached to the admin_init action hook.
  *
- * This call to register_setting() registers a validation callback, oriental_theme_options_validate(),
+ * This call to register_setting() registers a validation callback, cuteFrames_theme_options_validate(),
  * which is used when the option is saved, to ensure that our option values are complete, properly
  * formatted, and safe.
  *
  * We also use this function to add our theme option if it doesn't already exist.
  *
- * @since oriental 1.3.0
+ * @since cuteFrames 1.3.0
  */
 
-function oriental_theme_options_init() {
+function cuteFrames_theme_options_init() {
 
 	// If we have no options in the database, let's add them now.
-	if ( false === oriental_get_theme_options() )
-		add_option( 'oriental_theme_options', oriental_get_default_theme_options() );
+	if ( false === cuteFrames_get_theme_options() )
+		add_option( 'cuteFrames_theme_options', cuteFrames_get_default_theme_options() );
 
 	register_setting(
-		'oriental_options',       // Options group, see settings_fields() call in oriental_theme_options_render_page()
-		'oriental_theme_options', // Database option, see oriental_get_theme_options()
-		'oriental_theme_options_validate' // The sanitization callback, see oriental_theme_options_validate()
+		'cuteFrames_options',       // Options group, see settings_fields() call in cuteFrames_theme_options_render_page()
+		'cuteFrames_theme_options', // Database option, see cuteFrames_get_theme_options()
+		'cuteFrames_theme_options_validate' // The sanitization callback, see cuteFrames_theme_options_validate()
 	);
 
 	// Register our settings field group
@@ -37,37 +37,37 @@ function oriental_theme_options_init() {
 		'general', // Unique identifier for the settings section
 		'', // Section title (we don't want one)
 		'__return_false', // Section callback (we don't want anything)
-		'theme_options' // Menu slug, used to uniquely identify the page; see oriental_theme_options_add_page()
+		'theme_options' // Menu slug, used to uniquely identify the page; see cuteFrames_theme_options_add_page()
 	);
 
 	/* Register our individual settings fields */
 
-	add_settings_field( 'custom_css', __( 'Custom CSS', 'cuteFrames' ), 'oriental_settings_field_custom_css', 'theme_options', 'general' );
+	add_settings_field( 'custom_css', __( 'Custom CSS', 'cuteFrames' ), 'cuteFrames_settings_field_custom_css', 'theme_options', 'general' );
 
 	add_settings_field(
 		'support', // Unique identifier for the field for this section
-		__( 'Support oriental', 'vintage-camera' ), // Setting field label
-		'oriental_settings_field_support', // Function that renders the settings field
+		__( 'Support cuteFrames', 'cuteFrames' ), // Setting field label
+		'cuteFrames_settings_field_support', // Function that renders the settings field
 		'theme_options', // Menu slug, used to uniquely identify the page; see _s_theme_options_add_page()
 		'general' // Settings section. Same as the first argument in the add_settings_section() above
 	);
 
 }
-add_action( 'admin_init', 'oriental_theme_options_init' );
+add_action( 'admin_init', 'cuteFrames_theme_options_init' );
 
 /**
- * Change the capability required to save the 'oriental_options' options group.
+ * Change the capability required to save the 'cuteFrames_options' options group.
  *
- * @see oriental_theme_options_init() First parameter to register_setting() is the name of the options group.
- * @see oriental_theme_options_add_page() The edit_theme_options capability is used for viewing the page.
+ * @see cuteFrames_theme_options_init() First parameter to register_setting() is the name of the options group.
+ * @see cuteFrames_theme_options_add_page() The edit_theme_options capability is used for viewing the page.
  *
  * @param string $capability The capability used for the page, which is manage_options by default.
  * @return string The capability to actually use.
  */
-function oriental_option_page_capability( $capability ) {
+function cuteFrames_option_page_capability( $capability ) {
 	return 'edit_theme_options';
 }
-add_filter( 'option_page_capability_oriental_options', 'oriental_option_page_capability' );
+add_filter( 'option_page_capability_cuteFrames_options', 'cuteFrames_option_page_capability' );
 
 /**
  * Add our theme options page to the admin menu, including some help documentation.
@@ -76,42 +76,42 @@ add_filter( 'option_page_capability_oriental_options', 'oriental_option_page_cap
  *
 
  */
-function oriental_theme_options_add_page() {
+function cuteFrames_theme_options_add_page() {
 	$theme_page = add_theme_page(
-		__( 'Theme Options', 'oriental' ),   // Name of page
-		__( 'Theme Options', 'oriental' ),   // Label in menu
+		__( 'Theme Options', 'cuteFrames' ),   // Name of page
+		__( 'Theme Options', 'cuteFrames' ),   // Label in menu
 		'edit_theme_options',                    // Capability required
 		'theme_options',                         // Menu slug, used to uniquely identify the page
-		'oriental_theme_options_render_page' // Function that renders the options page
+		'cuteFrames_theme_options_render_page' // Function that renders the options page
 	);
 
 	if ( ! $theme_page )
 		return;
 }
-add_action( 'admin_menu', 'oriental_theme_options_add_page' );
+add_action( 'admin_menu', 'cuteFrames_theme_options_add_page' );
 
 
 /**
- * Returns the default options for oriental.
+ * Returns the default options for cuteFrames.
  *
 
  */
-function oriental_get_default_theme_options() {
+function cuteFrames_get_default_theme_options() {
 	$default_theme_options = array(
 		'custom_css' => '',
 		'support' => 0
 	);
 
-	return apply_filters( 'oriental_default_theme_options', $default_theme_options );
+	return apply_filters( 'cuteFrames_default_theme_options', $default_theme_options );
 }
 
 /**
- * Returns the options array for oriental.
+ * Returns the options array for cuteFrames.
  *
 
  */
-function oriental_get_theme_options() {
-	return get_option( 'oriental_theme_options', oriental_get_default_theme_options() );
+function cuteFrames_get_theme_options() {
+	return get_option( 'cuteFrames_theme_options', cuteFrames_get_default_theme_options() );
 }
 
 /**
@@ -119,15 +119,15 @@ function oriental_get_theme_options() {
  *
 
  */
-function oriental_settings_field_theme_style() {
-	$options = oriental_get_theme_options();
+function cuteFrames_settings_field_theme_style() {
+	$options = cuteFrames_get_theme_options();
 
-	foreach ( oriental_theme_style() as $button ) {
+	foreach ( cuteFrames_theme_style() as $button ) {
 	?>
 	<div class="layout">
 		<label class="description">
 			<img src="<?php echo get_template_directory_uri() ?>/images/ss/<?php echo $button['value']; ?>.png" alt="<?php echo $button['label']; ?> Style" /><br />
-			<input type="radio" name="oriental_theme_options[theme_style]" value="<?php echo esc_attr( $button['value'] ); ?>" <?php checked( $options['theme_style'], $button['value'] ); ?> />
+			<input type="radio" name="cuteFrames_theme_options[theme_style]" value="<?php echo esc_attr( $button['value'] ); ?>" <?php checked( $options['theme_style'], $button['value'] ); ?> />
 			<?php echo $button['label']; ?>
 		</label>
 	</div>
@@ -141,40 +141,40 @@ function oriental_settings_field_theme_style() {
  *
 
  */
-function oriental_settings_field_custom_css() {
-	$options = oriental_get_theme_options();
+function cuteFrames_settings_field_custom_css() {
+	$options = cuteFrames_get_theme_options();
 	?>
-	<textarea class="large-text" type="text" name="oriental_theme_options[custom_css]" id="custom_css" cols="50" rows="10" /><?php echo esc_textarea( $options['custom_css'] ); ?></textarea>
-	<label class="description" for="custom_css"><?php _e( 'Add any custom CSS rules here so they will persist through theme updates.', 'vintage-camera' ); ?></label>
+	<textarea class="large-text" type="text" name="cuteFrames_theme_options[custom_css]" id="custom_css" cols="50" rows="10" /><?php echo esc_textarea( $options['custom_css'] ); ?></textarea>
+	<label class="description" for="custom_css"><?php _e( 'Add any custom CSS rules here so they will persist through theme updates.', 'cuteFrames' ); ?></label>
 	<?php
 }
 
 /**
  * Renders the Support setting field.
  */
-function oriental_settings_field_support() {
-	$options = oriental_get_theme_options();
+function cuteFrames_settings_field_support() {
+	$options = cuteFrames_get_theme_options();
 
 	if ( $options['support'] !== 'on' || !isset( $options['support'] ) ) {
 
 	?>
-	<label for"vintage-camera-support">
+	<label>
 		<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PUKAB93RNE83S" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" alt="PayPal - The safer, easier way to pay online!" class="alignright"></a>
 
-		<?php _e( 'If you enjoy my themes, please consider making a secure donation using the PayPal button to your right. Anything is appreciated!', 'vintage-camera' ); ?>
+		<?php _e( 'If you enjoy my themes, please consider making a secure donation using the PayPal button to your right. Anything is appreciated!', 'cuteFrames' ); ?>
 
-		<br /><input type="checkbox" name="oriental_theme_options[support]" id="support" <?php checked( 'on', $options['support'] ); ?> />
+		<br /><input type="checkbox" name="cuteFrames_theme_options[support]" id="support" <?php checked( 'on', $options['support'] ); ?> />
 		<label class="description" for="support">
-			<?php _e( 'No, thank you! Dismiss this message.', 'vintage-camera' ); ?>
+			<?php _e( 'No, thank you! Dismiss this message.', 'cuteFrames' ); ?>
 		</label>
 	</label>
 	<?php
 	}
 	else { ?>
 		<label class="description" for="support">
-			<?php _e( 'Hide Donate Button', 'vintage-camera' ); ?>
+			<?php _e( 'Hide Donate Button', 'cuteFrames' ); ?>
 		</label>
-		<input type="checkbox" name="oriental_theme_options[support]" id="support" <?php checked( 'on', $options['support'] ); ?> />
+		<input type="checkbox" name="cuteFrames_theme_options[support]" id="support" <?php checked( 'on', $options['support'] ); ?> />
 
 	</td>
 
@@ -184,20 +184,20 @@ function oriental_settings_field_support() {
 }
 
 /**
- * Returns the options array for oriental.
+ * Returns the options array for cuteFrames.
  *
 
  */
-function oriental_theme_options_render_page() {
+function cuteFrames_theme_options_render_page() {
 	?>
 	<div class="wrap">
 		<?php screen_icon(); ?>
-		<h2><?php printf( __( '%s Theme Options', 'oriental' ), wp_get_theme() ); ?></h2>
+		<h2><?php printf( __( '%s Theme Options', 'cuteFrames' ), wp_get_theme() ); ?></h2>
 		<?php settings_errors(); ?>
 
 		<form method="post" action="options.php">
 			<?php
-				settings_fields( 'oriental_options' );
+				settings_fields( 'cuteFrames_options' );
 				do_settings_sections( 'theme_options' );
 				submit_button();
 			?>
@@ -210,7 +210,7 @@ function oriental_theme_options_render_page() {
 /**
  * Enqueue theme options styles
  */
-function oriental_admin_layout_styles( $hook_suffix ) {
+function cuteFrames_admin_layout_styles( $hook_suffix ) {
 
 	if ( $hook_suffix != 'appearance_page_theme_options' )
 		return; ?>
@@ -223,20 +223,20 @@ function oriental_admin_layout_styles( $hook_suffix ) {
 
 }
 
-add_action( 'admin_enqueue_scripts', 'oriental_admin_layout_styles' );
+add_action( 'admin_enqueue_scripts', 'cuteFrames_admin_layout_styles' );
 
 
 /**
  * Returns layout defaults
  */
-function oriental_get_layout_defaults() {
-	$options = oriental_get_theme_options();
+function cuteFrames_get_layout_defaults() {
+	$options = cuteFrames_get_theme_options();
 	$theme_style = $options['theme_style'];
 
-	$default_theme_options = oriental_get_default_theme_options();
+	$default_theme_options = cuteFrames_get_default_theme_options();
 	$default_theme_style = $default_theme_options['theme_style'];
 
-	$theme_style_values = oriental_theme_style();
+	$theme_style_values = cuteFrames_theme_style();
 
 	if ( $theme_style ) {
 		$defaults = $theme_style_values[$theme_style]['defaults'];
@@ -244,23 +244,23 @@ function oriental_get_layout_defaults() {
 		$defaults = $theme_style_values[$default_theme_style]['defaults'];
 	}
 
-	return apply_filters( 'oriental_get_layout_defaults', $defaults );
+	return apply_filters( 'cuteFrames_get_layout_defaults', $defaults );
 }
 
 
 /**
  * Sanitize and validate form input. Accepts an array, return a sanitized array.
  *
- * @see oriental_theme_options_init()
+ * @see cuteFrames_theme_options_init()
  * @todo set up Reset Options action
  *
 
  */
-function oriental_theme_options_validate( $input ) {
-	$output = $defaults = oriental_get_default_theme_options();
+function cuteFrames_theme_options_validate( $input ) {
+	$output = $defaults = cuteFrames_get_default_theme_options();
 
 	// The sample Theme Styles value must be in our array of Theme Styles values
-	if ( isset( $input['theme_style'] ) && array_key_exists( $input['theme_style'], oriental_theme_style() ) )
+	if ( isset( $input['theme_style'] ) && array_key_exists( $input['theme_style'], cuteFrames_theme_style() ) )
 		$output['theme_style'] = $input['theme_style'];
 
 	// The Support field should either be on or off
@@ -272,5 +272,5 @@ function oriental_theme_options_validate( $input ) {
 	if ( isset( $input['custom_css'] ) )
 		$output['custom_css'] = wp_filter_nohtml_kses($input['custom_css'] );
 
-	return apply_filters( 'oriental_theme_options_validate', $output, $input, $defaults );
+	return apply_filters( 'cuteFrames_theme_options_validate', $output, $input, $defaults );
 }
